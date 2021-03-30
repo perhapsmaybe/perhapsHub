06f65eb64d62187ecc2aa52fab034ba6ca0df9b3
perhapsmaybe <68890108+perhapsmaybe@users.noreply.github.com>
2021-03-30 00:46:35 -0700

    Update Console.lua

--- Console.lua
+++ Console.lua
@@ -15,10 +15,8 @@ SPLITHERE - Blade Quest
 
 SplitText = SplitText:split("SPLITHERE")
 
-_G.Finished = true
-_G.CancelLoop = false
-
-_G.Enabled = true
+local API = loadstring(game:HttpGet("https://raw.githubusercontent.com/perhapsmaybe/perhapsHub/master/CrossCompatibility/Main.lua"))()
+local Exploit = API.getexploit()
 
 -- // Script
 
@@ -28,7 +26,6 @@ function ClearConsole() rconsoleclear() end
 function ConsoleLine() rconsoleprint("\n") end
 
 function ChangeName(Text) rconsolename(Text) return Text end
-function AnimateName(Text, Game) spawn(function() if _G.Finished == true and ConsoleName == "" then for i, v in pairs(Text:split("")) do if _G.CancelLoop then _G.CancelLoop = false break end ConsoleName = ChangeName(ConsoleName..v) wait(0.1) if i == #Text then wait(0.15) ConsoleName = ChangeName(ConsoleName.." >") wait(0.25) ConsoleName = ChangeName(ConsoleName.."> ") for i1, v1 in pairs(Game:split("")) do if _G.CancelLoop then _G.CancelLoop = false break end ConsoleName = ChangeName(ConsoleName..v1) wait(0.1) if i1 == #Game then wait(1) for i2, v2 in pairs(ConsoleName:split("")) do ConsoleName = ChangeName(ConsoleName:sub(1, -2)) wait(0.1) end end end end end else repeat wait() until _G.Finished == true _G.CancelLoop = true AnimateName(Text, Game) end end) end 
 
 function ColoredText(Text) local CurrentName = "" CurrentColor = 0  for i, v in pairs(Text:split("")) do CurrentColor = CurrentColor + 1 if CurrentColor > #Colors then CurrentColor = 1 end rconsoleprint("@@"..Colors[CurrentColor].."@@") rconsoleprint(v) end end 
 function AnimateText(Text) rconsoleprint("@@LIGHT_GRAY@@") local CurrentName = "" rconsoleprint("\n") for i, v in pairs(Text:split("")) do CurrentName = CurrentName..v rconsoleprint(v) wait(0.1) end end 
@@ -41,6 +38,11 @@ ClearConsole()
 ColoredText(Title)
 
 rconsoleprint("\n")
+rconsoleprint("@@GREEN@")
+rconsoleprint("Loaded using ")
+rconsoleprint("@@CYAN@@")
+rconsoleprint(Exploit.."!")
+rconsoleprint("\n")
 rconsoleinfo("Games")
 rconsoleprint("@@GREEN@@")
 rconsoleprint(Games)
@@ -48,5 +50,5 @@ rconsoleprint(Games)
 local marketplaceService = game:GetService("MarketplaceService")
 local isSuccessful, info = pcall(marketplaceService.GetProductInfo, marketplaceService, game.PlaceId)
 if isSuccessful then
-  ChangeName("perhapsHub", info.Name)
+  ChangeName("perhapsHub >> "..info.Name)
 end
